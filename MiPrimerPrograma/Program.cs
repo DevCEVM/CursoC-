5333f0863e46818b9b46d2412e6a8ea97cb44777
Curso C# <ce.villegas92@gmail.com>
2026-02-25 10:59:49 -0600

    No entiendo qué estoy commiteando?

---
+++ MiPrimerPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MiPrimerPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Cristopher Villegas");
+            Console.Beep();
+        }
+    }
+}
